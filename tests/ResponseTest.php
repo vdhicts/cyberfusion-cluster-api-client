db084f711aaedddc81678e0f5a277562c8ca5b7a
Dick van der Heiden <info@vdhicts.nl>
2021-01-21 10:08:06 +0100

    Add test for response

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -18,4 +18,17 @@ class ResponseTest extends TestCase
         $this->assertSame('ok', $response->getData('data'));
         $this->assertNull($response->getData('foo'));
     }
+
+    public function testMutatingResponseData()
+    {
+        $response = new Response(200, 'Test', ['data' => 'ok']);
+
+        $this->assertArrayHasKey('data', $response->getData());
+        $this->assertSame('ok', $response->getData('data'));
+
+        $response->setData(['data' => 'fail']);
+
+        $this->assertArrayHasKey('data', $response->getData());
+        $this->assertSame('fail', $response->getData('data'));
+    }
 }
